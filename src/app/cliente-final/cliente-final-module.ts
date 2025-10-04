ee090040f8b028f10b0c0e3263ef9c646f739c24
isabela <escuisagar@gmail.com>
2025-10-03 23:06:31 -0500

    confirmacion-cita y todo funsional , mas generador de token

--- src/app/cliente-final/cliente-final-module.ts
+++ src/app/cliente-final/cliente-final-module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ClienteFinalRoutingModule } from './cliente-final-routing.module';
-
+import { ConfirmarCitaComponent } from './confirmar-cita/confirmar-cita.component';
 
 @NgModule({
+  declarations: [],  // Componentes standalone NO van aquí
   imports: [
     CommonModule,
-    ClienteFinalRoutingModule
+    ClienteFinalRoutingModule,
+    ConfirmarCitaComponent  // Componentes standalone van en imports
   ]
 })
 export class ClienteFinalModule {}
