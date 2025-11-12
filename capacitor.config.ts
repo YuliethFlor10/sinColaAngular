74cb832ff86cff01ee9f49df0a129ddc9d2b1cbc
YuliethFlor10 <yulieth.flor@gmail.com>
2025-11-12 18:39:42 -0500

    Implementacion android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sinCola.miapp',
+  appName: 'sinCola',
+  webDir: 'dist/sinColaAngular/browser'
+};
+
+export default config;
